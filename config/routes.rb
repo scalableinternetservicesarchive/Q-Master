bc247fb4a32b0c40e3a992c43c1158fcbfb21aca
BoyuanFeng <bfeng9@wisc.edu>
2018-10-16 17:43:20 -0700

    Added posting articles and comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+
+
+  root 'welcome#index'
 end
