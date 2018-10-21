161ef0a4d833b2ebe4dae9c85290916b342d372f
Zimu YANG <zimuyang@169-231-110-104.wireless.ucsb.edu>
2018-10-21 00:00:26 -0700

    Relation between users and articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,5 @@
 class ArticlesController < ApplicationController
   
-  http_basic_authenticate_with name: "boyuan", password: "secret", except: [:index, :show]
-
 
   def index
     @articles = Article.all
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_21_022459) do
+ActiveRecord::Schema.define(version: 2018_10_21_024759) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id", "created_at"], name: "index_articles_on_user_id_and_created_at"
   end
 
   create_table "comments", force: :cascade do |t|
