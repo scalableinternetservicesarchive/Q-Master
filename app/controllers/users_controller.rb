44b565f54fcaaaca97865e7d536e4f0a3d2c7fc7
Zimu YANG <zimuyang@169-231-109-54.wireless.ucsb.edu>
2018-10-22 17:20:43 -0700

    fix some issues with article posting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+	 @article = @user.articles.build()
 	 @articles = @user.articles.paginate(page: params[:page])
   end
 
