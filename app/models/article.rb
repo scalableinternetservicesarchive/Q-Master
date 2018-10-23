79b8022ef6fd5459524642dd75f8850f3057067c
Zimu YANG <zimuyang@169-231-109-54.wireless.ucsb.edu>
2018-10-22 17:00:25 -0700

    fix

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,8 +2,6 @@ class Article < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   default_scope -> { order('created_at DESC')}
-  validates :title, presence: true,
-                    length: { minimum: 5 }
   validates :user_id, presence: true
   validates :text, presence: true,
   						 length: { maximum: 140 }
