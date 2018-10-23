a9ca0197f4bd9a27788d54ad33b60b335fbf1f70
Zimu YANG <zimuyang@MacBook-Pro-69.local>
2018-10-22 21:06:31 -0700

    fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,9 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
-
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
+	 @comment.user_id = @article.user.id
     redirect_to article_path(@article)
   end
 
@@ -18,7 +17,7 @@ class CommentsController < ApplicationController
 
   private
     def comment_params
-      params.require(:comment).permit(:commenter, :body)
+      params.require(:comment).permit(:body)
     end
 
 end
