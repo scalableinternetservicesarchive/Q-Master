3d437156ef496592f4d3326c93668b235eaa2a2e
SYshuyang <shuyang1995@ucsb.edu>
2018-10-31 15:18:07 -0700

    add resize feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,4 +8,12 @@ class Article < ApplicationRecord
   validates :question_id, presence: true
   validates :text, presence: true,
   						 length: { maximum: 140 }
+  validate :picture_size
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, "should be less than 5MB")
+    end
+  end
+
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,4 +1,6 @@
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -11,6 +13,10 @@ class PictureUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+  def extension_whitelist
+    %w(jpg jpeg gif png)
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
