a47323a6df39d16dd89aa75318886879797bbb50
Niffery <980712613@qq.com>
2018-11-06 13:57:58 -0800

    seeding

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,7 +4,7 @@ class Question < ApplicationRecord
 	default_scope -> { order('created_at DESC') }
 	validates :user_id, presence: true
 	validates :content , length: { minimum:5 }
-
+	validates :title , length: { minimum:5 }
 	def self.from_users_followed_by(user)
 		followed_user_ids = "SELECT followed_id FROM relationships WHERE follower_id = :user_id"
 		where("user_id IN (:#{followed_user_ids}) OR user_id = :user_id", user_id: user.id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_list = [
+	[ "zimu", "zimu@ucsb.com", "123456", "123456" ],
+	[ "boyuan", "boyuan@ucsb.com", "123456", "123456" ],
+	[ "shuyang", "shuyang@ucsb.com", "123456", "123456" ],
+	[ "april", "april@ucsb.com", "123456", "123456" ],
+]
+
+user_list.each do |name, email, password, password_confirmation|
+	User.create( name: name, email: email, password: password, password_confirmation: password_confirmation)
+end
+
+
+User.find(1).questions.create( title: "why there are so many programmers?", content: "I'm talking about the fact.")
+User.find(2).questions.create( title: "why there are so many programmers?", content: "I'm talking about the fact.")
+User.find(3).questions.create( title: "why there are so many programmers?", content: "I'm talking about the fact.")
+User.find(4).questions.create( title: "why there are so many programmers?", content: "I'm talking about the fact.")
