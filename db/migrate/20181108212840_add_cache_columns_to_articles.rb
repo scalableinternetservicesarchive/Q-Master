55fc48b7064ea742107e2bee35e27d31afbd6dd2
Niffery <980712613@qq.com>
2018-11-08 13:31:47 -0800

    fix

--- db/migrate/20181108212840_add_cache_columns_to_articles.rb
+++ db/migrate/20181108212840_add_cache_columns_to_articles.rb
@@ -1,6 +1,6 @@
-class AddCachedToArticles < ActiveRecord::Migration[5.2]
+class AddCacheColumnsToArticles < ActiveRecord::Migration[5.2]
   def change
-    change_table :articles do |t|
+  	 change_table :articles do |t|
       t.integer :cached_votes_total, default: 0
       t.integer :cached_votes_score, default: 0
       t.integer :cached_votes_up, default: 0
@@ -8,6 +8,6 @@ class AddCachedToArticles < ActiveRecord::Migration[5.2]
       t.integer :cached_weighted_score, default: 0
       t.integer :cached_weighted_total, default: 0
       t.float :cached_weighted_average, default: 0.0
-	 end
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_08_190903) do
+ActiveRecord::Schema.define(version: 2018_11_08_212840) do
 
   create_table "articles", force: :cascade do |t|
     t.text "text"
@@ -64,10 +64,6 @@ ActiveRecord::Schema.define(version: 2018_11_08_190903) do
     t.string "password_digest"
     t.string "remember_token"
     t.boolean "admin", default: false
-    t.string "avatar_file_name"
-    t.string "avatar_content_type"
-    t.integer "avatar_file_size"
-    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
