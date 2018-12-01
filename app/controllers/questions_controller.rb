1927f1e808774d4438b6fc8db3573a56b56cf5b7
Niffery <980712613@qq.com>
2018-12-01 10:35:57 -0800

    after make sql optimization to question_index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,11 +4,11 @@ class QuestionsController < ApplicationController
   before_action :correct_user, only: :destroy
 
   def index
-   # without includes(:user) is before and with is after
-#    @q = Question.includes(:user).ransack(params[:q])
-#	 @questions = @q.result.paginate(page: params[:page])
-    @q = Question.ransack(params[:q])
+   #without includes(:user) is before and with is after
+    @q = Question.includes(:user).ransack(params[:q])
 	 @questions = @q.result.paginate(page: params[:page])
+#    @q = Question.ransack(params[:q])
+#	 @questions = @q.result.paginate(page: params[:page])
 	 respond_to do |format|
 		format.html
 		format.js
