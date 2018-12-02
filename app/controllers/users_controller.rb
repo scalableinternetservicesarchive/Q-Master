f873d0b19865a86615d094162502b0496d4ff5f2
Niffery <980712613@qq.com>
2018-12-02 11:18:32 -0800

    CheckPoint after add client-side caching to user_index and user_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,10 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
+=begin
 	 @q = User.ransack(params[:q])
 	 @users = @q.result.paginate(page: params[:page], per_page: 10)
-=begin
+=end
     if stale?(User.all)
 	   @q = User.ransack(params[:q])
 	   @users = @q.result.paginate(page: params[:page], per_page: 10)
@@ -16,16 +17,14 @@ class UsersController < ApplicationController
 			format.js
 		end
 	 end
-=end
   end
 
   def show
-
     @user = User.find(params[:id])
 	 @question = @user.questions.build()
 	 @articles = @user.articles.paginate(page: params[:articles_page], per_page: 10 )
 	 @questions = @user.questions.paginate(page: params[:questions_page], per_page: 10 )
-#fresh_when([@user, @articles, @questions])
+	 fresh_when([@user, @articles, @questions])
   end
 
   def new
